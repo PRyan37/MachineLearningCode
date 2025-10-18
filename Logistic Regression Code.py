15ba84d7c0f8d3b97ba351dbb48d873926997ddd
Pryan37 <P.ryan37@universityofgalway.ie>
2025-10-18 20:45:29 +0100

    Models now used on training data

--- Logistic Regression Code.py
+++ Logistic Regression Code.py
@@ -2,8 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.preprocessing import StandardScaler
 training_file="wildfires_training.csv"
 test_file="wildfires_test.csv"
 independent_cols=['year', 'temp', 'humidity', 'rainfall', 'drought_code', 'buildup_index', 'day','month','wind_speed']
@@ -44,17 +42,24 @@ accuracy_training = metrics.accuracy_score(y_training, predictions_training)
 accuracy_test = metrics.accuracy_score(y_test, predictions_test)
 print("Accuracy on training data:",accuracy_training)
 print("Accuracy on test data:",accuracy_test)
+
+
 penalty_list=['l1', 'l2', 'elasticnet', None]
 c_values=[0.01, 0.1, 1, 10, 100]
 print("C values:", c_values)
 print("Penalty values:", penalty_list)
+
+
 print("\n ------------TESTING C------------- \n")
 
 for c in c_values:
     model = LogisticRegression(C=c, max_iter=20000)
     model.fit(X_training, y_training)
+    predictions_training = model.predict(X_training)
     predictions_test = model.predict(X_test)
+    accuracy_training = metrics.accuracy_score(y_training, predictions_training)
     accuracy_test = metrics.accuracy_score(y_test, predictions_test)
+    print("C: ",c,"Accuracy on training data:",accuracy_training)
     print("C:", c, " - Accuracy on test data:", accuracy_test)
 
 print("\n ------------TESTING PENALTY------------- \n")
@@ -68,21 +73,42 @@ for penalty in penalty_list:
         model = LogisticRegression(penalty=penalty,solver="liblinear", max_iter=20000 )
     model.fit(X_training, y_training)
     predictions_test = model.predict(X_test)
+    predictions_training = model.predict(X_training)
+    accuracy_training = metrics.accuracy_score(y_training, predictions_training)
     accuracy_test = metrics.accuracy_score(y_test, predictions_test)
+    print("Penalty: ",penalty,"Accuracy on training data:",accuracy_training)
     print("Penalty:", penalty, " - Accuracy on test data:", accuracy_test)
 
 penalty_list_no_none=['l1', 'l2', 'elasticnet']
-l1_accuracy=[]
-l2_accuracy=[]
-elasticnet_accuracy=[]
-best_penalty=""
-best_c=0
-best_accuracy=0
-worst_penalty=""
-worst_c=0
-worst_accuracy=1
+
+best_penalty_training=""
+best_c_training=0
+best_accuracy_training=0
+worst_penalty_training=""
+worst_c_training=0
+worst_accuracy_training=1
+
+
+
+l1_accuracy_training=[]
+l2_accuracy_training=[]
+elasticnet_accuracy_training=[]
+
+
+best_penalty_test=""
+best_c_test=0
+best_accuracy_test=0
+worst_penalty_test=""
+worst_c_test=0
+worst_accuracy_test=1
+
+l1_accuracy_test=[]
+l2_accuracy_test=[]
+elasticnet_accuracy_test=[]
+
 count=0
-total_accuracy=0
+total_accuracy_test=0
+total_accuracy_training=0
 
 print("\n ------------TESTING BOTH C AND PENALTY------------- \n")
 for c in c_values:
@@ -97,45 +123,96 @@ for c in c_values:
 
         model.fit(X_training, y_training)
         predictions_test = model.predict(X_test)
+        predictions_training = model.predict(X_training)
+        accuracy_training = metrics.accuracy_score(y_training, predictions_training)
         accuracy_test = metrics.accuracy_score(y_test, predictions_test)
+
         if penalty == 'l1':
-            l1_accuracy.append(accuracy_test)
+            l1_accuracy_training.append(accuracy_training)
         elif penalty == 'l2':
-            l2_accuracy.append(accuracy_test)
+            l2_accuracy_training.append(accuracy_training)
         elif penalty == 'elasticnet':
-            elasticnet_accuracy.append(accuracy_test)
-        if accuracy_test > best_accuracy:
-            best_accuracy=accuracy_test
-            best_penalty=penalty
-            best_c=c
-        if accuracy_test < worst_accuracy:
-            worst_accuracy=accuracy_test
-            worst_penalty=penalty
-            worst_c=c
+            elasticnet_accuracy_training.append(accuracy_training)
+        if accuracy_training > best_accuracy_training:
+            best_accuracy_training=accuracy_training
+            best_penalty_training=penalty
+            best_c_training=c
+        if accuracy_training < worst_accuracy_training:
+            worst_accuracy_training=accuracy_training
+            worst_penalty_training=penalty
+            worst_c_training=c
+
+
+        if penalty == 'l1':
+            l1_accuracy_test.append(accuracy_test)
+        elif penalty == 'l2':
+            l2_accuracy_test.append(accuracy_test)
+        elif penalty == 'elasticnet':
+            elasticnet_accuracy_test.append(accuracy_test)
+        if accuracy_test > best_accuracy_test:
+            best_accuracy_test=accuracy_test
+            best_penalty_test=penalty
+            best_c_test=c
+        if accuracy_test < worst_accuracy_test:
+            worst_accuracy_test=accuracy_test
+            worst_penalty_test=penalty
+            worst_c_test=c
+
+
+
+
         count+=1
-        total_accuracy=total_accuracy+accuracy_test
+        total_accuracy_test=total_accuracy_test+accuracy_test
+        total_accuracy_training=total_accuracy_training+accuracy_training
+        print("Penalty:", penalty, "| C: ",c," - Accuracy on training data:", accuracy_training)
         print("Penalty:", penalty, "| C: ",c," - Accuracy on test data:", accuracy_test)
 
-model = LogisticRegression(penalty=None, solver='lbfgs', max_iter=20000)
+model = LogisticRegression(penalty=None, max_iter=20000)
 model.fit(X_training, y_training)
+predictions_training = model.predict(X_training)
 predictions_test = model.predict(X_test)
-none_accuracy = metrics.accuracy_score(y_test, predictions_test)
-print("Penalty: None - Accuracy on test data:", none_accuracy)
+none_accuracy_training = metrics.accuracy_score(y_training, predictions_training)
+none_accuracy_test = metrics.accuracy_score(y_test, predictions_test)
+print("Penalty: None - Accuracy on training data:", none_accuracy_training)
+print("Penalty: None - Accuracy on test data:", none_accuracy_test)
 
-print("\n Best Penalty:", best_penalty, "| Best C:", best_c, " | Best Accuracy:", best_accuracy)
-print(" Worst Penalty:", worst_penalty, "| Worst C:", worst_c, " | Worst Accuracy:", worst_accuracy)
-print("Average Accuracy:", total_accuracy/count)
 
-plt.plot(c_values, l1_accuracy, marker='o', label='L1')
-plt.plot(c_values, l2_accuracy, marker='o', label='L2')
-plt.plot(c_values, elasticnet_accuracy, marker='o', label='ElasticNet')
-plt.axhline(y=none_accuracy, color='red', label='None')
+print("------------TRAINING RESULTS----------- \n")
+print("\n Best Penalty:", best_penalty_training, "| Best C:", best_c_training, " | Best Accuracy:", best_accuracy_training)
+print(" Worst Penalty:", worst_penalty_training, "| Worst C:", worst_c_training, " | Worst Accuracy:", worst_accuracy_training)
+print("Average Accuracy:", total_accuracy_training/count)
+
+print("------------TEST RESULTS----------- \n")
+print("\n Best Penalty:", best_penalty_test, "| Best C:", best_c_test, " | Best Accuracy:", best_accuracy_test)
+print(" Worst Penalty:", worst_penalty_test, "| Worst C:", worst_c_test, " | Worst Accuracy:", worst_accuracy_test)
+print("Average Accuracy:", total_accuracy_test/count)
+
+plt.plot(c_values, l1_accuracy_training, marker='o', label='L1')
+plt.plot(c_values, l2_accuracy_training, marker='o', label='L2')
+plt.plot(c_values, elasticnet_accuracy_training, marker='o', label='ElasticNet')
+plt.axhline(y=none_accuracy_training, color='red', label='None')
+
+
+plt.xscale('log')
+plt.xlabel('C (Inverse Regularization Strength)')
+plt.ylabel('Accuracy on Training Data')
+plt.title('Effect of C and Penalty on Logistic Regression Accuracy (TRAINING)')
+plt.legend()
+plt.grid(True, which='major', linestyle='--', alpha=0.6)
+plt.tight_layout()
+plt.show()
+
+
+plt.plot(c_values, l1_accuracy_test, marker='o', label='L1')
+plt.plot(c_values, l2_accuracy_test, marker='o', label='L2')
+plt.plot(c_values, elasticnet_accuracy_test, marker='o', label='ElasticNet')
+plt.axhline(y=none_accuracy_test, color='red', label='None')
 
 
 plt.xscale('log')
 plt.xlabel('C (Inverse Regularization Strength)')
 plt.ylabel('Accuracy on Test Data')
-plt.title('Effect of C and Penalty on Logistic Regression Accuracy')
+plt.title('Effect of C and Penalty on Logistic Regression Accuracy (TEST)')
 plt.legend()
 plt.grid(True, which='major', linestyle='--', alpha=0.6)
 plt.tight_layout()
--- Random Forest Code.py
+++ Random Forest Code.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.preprocessing import StandardScaler
 training_file="wildfires_training.csv"
 test_file="wildfires_test.csv"
 independent_cols=['year', 'temp', 'humidity', 'rainfall', 'drought_code', 'buildup_index', 'day','month','wind_speed']
@@ -34,51 +33,190 @@ print(y_test.head())
 print(y_test.shape)
 
 
-model = RandomForestClassifier()
+model = RandomForestClassifier(random_state=43)
 model.fit(X_training, y_training)
 
 predictions_training = model.predict(X_training)
 predictions_test = model.predict(X_test)
 
-accuracy_training = metrics.accuracy_score(y_training, predictions_training)
-accuracy_test = metrics.accuracy_score(y_test, predictions_test)
-print("Accuracy on training data:",accuracy_training)
-print("Accuracy on test data:",accuracy_test)
+default_accuracy_training = metrics.accuracy_score(y_training, predictions_training)
+default_accuracy_test = metrics.accuracy_score(y_test, predictions_test)
+print("---------DEFAULT MODEL RESULTS---------")
+print("Accuracy on training data:",default_accuracy_training)
+print("Accuracy on test data:",default_accuracy_test)
 
 print("\n ------------TESTING N_ESTIMATORS------------ \n")
 n_estimator_values=[100, 200, 300, 400, 500]
 for n_estimator in n_estimator_values:
-    model = RandomForestClassifier(n_estimators=n_estimator)
+    model = RandomForestClassifier(n_estimators=n_estimator, random_state=43)
     model.fit(X_training, y_training)
+    predictions_training = model.predict(X_training)
     predictions_test = model.predict(X_test)
+    accuracy_training = metrics.accuracy_score(y_training, predictions_training)
     accuracy_test = metrics.accuracy_score(y_test, predictions_test)
+    print("n_estimators:", n_estimator, " - Accuracy on training data:", accuracy_training)
     print("n_estimators:", n_estimator, " - Accuracy on test data:", accuracy_test)
 
 print("\n ----------TESTING MAX_DEPTH----------- \n")
-max_depth_values=[5,10,15,20,25,30]
+criterion_values=['gini', 'entropy', 'log_loss']
+max_depth_values=[3, 6, 9, 12, 15, 18,None]
 for max_depth in max_depth_values:
-    model = RandomForestClassifier(max_depth=max_depth)
+    model = RandomForestClassifier(max_depth=max_depth,random_state=43)
     model.fit(X_training, y_training)
+    predictions_training = model.predict(X_training)
     predictions_test = model.predict(X_test)
+    accuracy_training = metrics.accuracy_score(y_training, predictions_training)
     accuracy_test = metrics.accuracy_score(y_test, predictions_test)
+    print("max_depth:", max_depth, " - Accuracy on training data:", accuracy_training)
     print("max_depth:", max_depth, " - Accuracy on test data:", accuracy_test)
 
-best_accuracy=0
-best_max_depth=0
-best_n_estimator=0
+best_accuracy_training=0
+best_max_depth_training=0
+best_n_estimator_training=0
+worst_accuracy_training=1
+worst_max_depth_training=0
+worst_n_estimator_training=0
+total_accuracy_training=0
+
+max_depth_list_training = []
+n_estimators_list_training = []
+accuracy_list_training = []
+
+
+
+best_accuracy_test=0
+best_max_depth_test=0
+best_n_estimator_test=0
+worst_accuracy_test=1
+worst_max_depth_test=0
+worst_n_estimator_test=0
+total_accuracy_test=0
+
+max_depth_list_test = []
+n_estimators_list_test = []
+accuracy_list_test = []
+
+
+count=0
 
 print("\n ------------TESTING BOTH MAX_DEPTH AND N_ESTIMATORS------------- \n")
 for max_depth in max_depth_values:
     for n_estimator in n_estimator_values:
-        model = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimator,random_state=42)
+        model = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimator, random_state=43)
         model.fit(X_training, y_training)
+        predictions_training = model.predict(X_training)
         predictions_test = model.predict(X_test)
+        accuracy_training = metrics.accuracy_score(y_training, predictions_training)
         accuracy_test = metrics.accuracy_score(y_test, predictions_test)
 
-        if accuracy_test>=best_accuracy:
-            best_accuracy=accuracy_test
-            best_max_depth=max_depth
-            best_n_estimator=n_estimator
+        if max_depth is None:
+            max_depth_list_training.append('None')
+        else:
+            max_depth_list_training.append(max_depth)
+        n_estimators_list_training.append(n_estimator)
+        accuracy_list_training.append(accuracy_training)
+
+        if accuracy_training>=best_accuracy_training:
+            best_accuracy=accuracy_training
+            best_max_depth_training=max_depth
+            best_n_estimator_training=n_estimator
+        if accuracy_training<=worst_accuracy_training:
+            worst_accuracy_training=accuracy_test
+            worst_max_depth_training=max_depth
+            worst_n_estimator_training=n_estimator
+
+
+        if max_depth is None:
+            max_depth_list_test.append('None')
+        else:
+            max_depth_list_test.append(max_depth)
+        n_estimators_list_test.append(n_estimator)
+        accuracy_list_test.append(accuracy_test)
+
+        if accuracy_test>=best_accuracy_test:
+            best_accuracy_test=accuracy_test
+            best_max_depth_test=max_depth
+            best_n_estimator_test=n_estimator
+        if accuracy_test<=worst_accuracy_test:
+            worst_accuracy_test=accuracy_test
+            worst_max_depth_test=max_depth
+            worst_n_estimator_test=n_estimator
+        count=count+1
+        total_accuracy_training = total_accuracy_training + accuracy_training
+        total_accuracy_test=total_accuracy_test+accuracy_test
         print("max_depth:", max_depth, " n_estimators:", n_estimator, " - Accuracy on test data:", accuracy_test)
 
-print("The best accuracy is ",best_accuracy," with max_depth=",best_max_depth," and n_estimators=",best_n_estimator)
+print("------------TRAINING RESULTS----------- \n")
+print("The best accuracy is ",best_accuracy_training," with max_depth=",best_max_depth_training," and n_estimators=",best_n_estimator_training)
+print("The worst accuracy is ",worst_accuracy_training," with max_depth=",worst_max_depth_training," and n_estimators=",worst_n_estimator_training)
+print("The average accuracy is ",total_accuracy_training/count)
+print("Default training accuracy is ",default_accuracy_training)
+
+print("------------TEST RESULTS----------- \n")
+print("The best accuracy is ",best_accuracy_test," with max_depth=",best_max_depth_test," and n_estimators=",best_n_estimator_test)
+print("The worst accuracy is ",worst_accuracy_test," with max_depth=",worst_max_depth_test," and n_estimators=",worst_n_estimator_test)
+print("The average accuracy is ",total_accuracy_test/count)
+print("Default test accuracy is ",default_accuracy_test)
+
+df = pd.DataFrame({
+    'max_depth': max_depth_list_training,
+    'n_estimators': n_estimators_list_training,
+    'accuracy': accuracy_list_training
+})
+# plot
+
+
+pivot = df.pivot(index='max_depth', columns='n_estimators', values='accuracy')
+plt.figure(figsize=(8, 5))
+sns.heatmap(pivot, annot=True,cmap='mako', fmt=".2f")
+plt.title("The Effect of max_depth and n_estimators on Random Forest Accuracy (TRAINING)")
+plt.ylabel("max_depth")
+plt.xlabel("n_estimators")
+plt.show()
+
+df = pd.DataFrame({
+    'max_depth': max_depth_list_test,
+    'n_estimators': n_estimators_list_test,
+    'accuracy': accuracy_list_test
+})
+# plot
+
+
+pivot = df.pivot(index='max_depth', columns='n_estimators', values='accuracy')
+plt.figure(figsize=(8, 5))
+sns.heatmap(pivot, annot=True,cmap='mako', fmt=".2f")
+plt.title("The Effect of max_depth and n_estimators on Random Forest Accuracy (TEST)")
+plt.ylabel("max_depth")
+plt.xlabel("n_estimators")
+plt.show()
+# print(model.feature_importances_)
+#
+# criteria = ['gini', 'entropy', 'log_loss']
+# n_estimators = [100, 200, 300, 400, 500]
+#
+# # Accuracy results
+# accuracies = {
+#     'gini': [0.84, 0.84, 0.82, 0.84, 0.84],
+#     'entropy': [0.86, 0.84, 0.84, 0.84, 0.86],
+#     'log_loss': [0.86, 0.84, 0.84, 0.84, 0.86]
+# }
+#
+# # Plot each criterion as a line
+# plt.figure(figsize=(8, 5))
+# for criterion in criteria:
+#     plt.plot(n_estimators, accuracies[criterion], marker='o', label=criterion)
+#
+# # Labels and title
+# plt.title("Random Forest Accuracy vs. Number of Estimators")
+# plt.xlabel("Number of Estimators (n_estimators)")
+# plt.ylabel("Accuracy on Test Data")
+# plt.xticks(n_estimators)
+# plt.ylim(0.8, 0.9)
+# plt.grid(True, linestyle='--', alpha=0.6)
+# plt.legend(title="Criterion")
+#
+#
+# plt.show()
+
+
+# ✅ after loop: create a dataframe for plotting
